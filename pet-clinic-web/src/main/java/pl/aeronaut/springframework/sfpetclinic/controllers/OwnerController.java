07a39e8a445a4d4d2eefbd23e18bdb94ac332ddd
morgh80 <m@mtaksamo100>
2018-09-17 21:36:04 +0200

    Html Thymeleaf Model populated, refactored map implemantation

---
+++ pet-clinic-web/src/main/java/pl/aeronaut/springframework/sfpetclinic/controllers/OwnerController.java
@@ -0,0 +1,23 @@
+package pl.aeronaut.springframework.sfpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.aeronaut.springframework.sfpetclinic.services.OwnerService;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"index", "index.html", "", "/"})
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+        return "owners/index";
+    }
+}
